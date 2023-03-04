4b979d4b8b9b02ed8f565f22d55486f4f6206b63
Gugan <110531967+kokkigugan@users.noreply.github.com>
2023-03-04 13:38:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { marked } from "marked";
+import "./App.scss";
+import Prism from 'prismjs';
+marked.setOptions({
+  breaks: true,
+  highlight: function (code) {
+    return Prism.highlight(code, Prism.languages.javascript, 'javascript');
+  }
+});
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+  return `<a target="_blank" href="${href}">${text}</a>`;
+};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: placeholder,
+      editorMaximized: false,
+      previewMaximized: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleEditorMaximize = this.handleEditorMaximize.bind(this);
+    this.handlePreviewMaximize = this.handlePreviewMaximize.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      markdown: e.target.value
+    });
+  }
+  handleEditorMaximize() {
+    this.setState({
+      editorMaximized: !this.state.editorMaximized
+    });
+  }
+  handlePreviewMaximize() {
+    this.setState({
+      previewMaximized: !this.state.previewMaximized
+    });
+  }
+  render() {
+    const classes = this.state.editorMaximized
+      ? ['editorWrap maximized', 'previewWrap hide', 'fa fa-compress']
+      : this.state.previewMaximized
+      ? ['editorWrap hide', 'previewWrap maximized', 'fa fa-compress']
+      : ['editorWrap', 'previewWrap', 'fa fa-arrows-alt'];
+    return (
+      <div>
+        <div className={classes[0]}>
+          <Toolbar
+            icon={classes[2]}
+            onClick={this.handleEditorMaximize}
+            text="Editor"
+          />
+          <Editor markdown={this.state.markdown} onChange={this.handleChange} />
+        </div>
+        <div className="converter" />
+        <div className={classes[1]}>
+          <Toolbar
+            icon={classes[2]}
+            onClick={this.handlePreviewMaximize}
+            text="Previewer"
+          />
+          <Preview markdown={this.state.markdown} />
+        </div>
+      </div>
+    );
+  }
+}
 
-function App() {
+const Toolbar = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="toolbar">
+      <i className="fa fa-free-code-camp" title="no-stack-dub-sack" />
+      {props.text}
+      <i className={props.icon} onClick={props.onClick} />
     </div>
   );
-}
+};
+
+const Editor = (props) => {
+  return (
+    <textarea
+      id="editor"
+      onChange={props.onChange}
+      type="text"
+      value={props.markdown}
+    />
+  );
+};
+
+const Preview = (props) => {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: marked(props.markdown, { renderer: renderer })
+      }}
+      id="preview"
+    />
+  );
+};
+
+const placeholder = 'hi myself Gugan';
+
 
 export default App;
